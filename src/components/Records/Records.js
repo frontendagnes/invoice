50bd3b86304e53abc87b215be9f930a9768d18c3
Agnieszka Kaminska <agnieszka.kaminska@ksiegarnia.edu.pl>
2022-03-16 16:08:47 +0100

    adding the records components functionality

--- src/components/Records/Records.js
+++ src/components/Records/Records.js
@@ -1,33 +1,44 @@
 import React, { useEffect, useState, useCallback } from "react";
 import "./Records.css";
-// components
-import InvoicesItem from "../InvoicesItem/InvoicesItem";
 import { getTotal } from "../../assets/functions";
 
 function Records({ data }) {
-  const [totalDay, setTotalDay] = useState([]);
   const [cumTotal, setCumTotal] = useState([]);
+  const [totalMnoth, setTotalMonth] = useState([]);
   const [number, setNumber] = useState(new Date().getMonth() + 1);
+  const [months, setMonths] = useState([
+    "Styczeń",
+    "Luty",
+    "Marzec",
+    "Kwiecień",
+    "Maj",
+    "Czerwiec",
+    "Lipiec",
+    "Sierpień",
+    "Wrzesień",
+    "Październik",
+    "Listopad",
+    "Grudzień",
+    "Podsumowanie roku",
+  ]);
 
-  const getTotalMonth = useCallback(() => {
+  const summaryYearId = months.indexOf("Podsumowanie roku");
+
+  const sumMonth = (num) => {
     let i = 0;
     let arr = [];
+
     while (i < data.length) {
       let date = new Date(data[i].data.date).getMonth() + 1;
-      if (date === number) {
+      if (date === num) {
         data[i].data.products.map((product) =>
           arr.push({ worth: product.worth })
         );
       }
       i++;
     }
-
-    setTotalDay(arr);
-  }, [data, number]);
-
-  useEffect(() => {
-    getTotalMonth();
-  }, [getTotalMonth]);
+    return getTotal(arr);
+  };
 
   const cumulativeTotal = useCallback(() => {
     let i = 0;
@@ -40,7 +51,7 @@ function Records({ data }) {
       }
       i++;
     }
-    
+
     let num = 0;
     let next = [];
     for (let i = 0; i < arr.length; i++) {
@@ -58,52 +69,119 @@ function Records({ data }) {
     let num = amount + 1;
     setNumber(num);
   };
-  const printButton = () => {
-    const months = [
-      "Styczeń",
-      "Luty",
-      "Marzec",
-      "Kwiecień",
-      "Maj",
-      "Czerwiec",
-      "Lipiec",
-      "Sierpień",
-      "Wrzesień",
-      "Październik",
-      "Listopad",
-      "Grudzień",
-    ];
+
+  const summaryYear = useCallback(() => {
+    let totalArr = [];
+    months
+      .filter((_, index) => index !== summaryYearId)
+      .forEach((_, index) => {
+        totalArr.push(sumMonth(index + 1));
+      });
+    setTotalMonth(totalArr);
+  }, [data]);
+
+  useEffect(() => {
+    summaryYear();
+  }, [summaryYear]);
+
+  const totalYear = () => {
+    return totalMnoth.reduce((amount, item) => item + amount, 0);
+  };
+  const printButtons = () => {
     return months.map((item, index) => (
-      <button key={index} type="button" onClick={() => numberChange(index)}>
+      <div
+        key={index + 1}
+        type="button"
+        className="records__button"
+        onClick={() => numberChange(index)}
+        style={{ background: index === number - 1 ? "#3f4d70" : "#1976d2" }}
+      >
         {item}
-      </button>
+      </div>
     ));
   };
   return (
     <div className="records">
-      <div className="records__buttons">
-        {printButton()}
-        {/* <button type="button" onClick={cumulativeTotal}>cummulative Total</button> */}
-      </div>
-      <div className="records__total">total month:{getTotal(totalDay)} </div>
-      <ul>
-        {data
-          .filter((item) => new Date(item.data.date).getMonth() + 1 === number)
-          .map((item, index) => (
-            <li key={index}>
-              <div>{index + 1}</div>
-              <div>{item.data.number}</div>
-              <div>{item.data.date}</div>
-              <div>
-                {item.data.products.map((pr) => (
-                  <div>{pr.worth}</div>
-                ))}
+      <div className="records__buttons">{printButtons()}</div>
+      <div className="records__wrapper">
+      {number - 1 !== summaryYearId ? (
+        <table>
+          <caption>
+            <div className="records__total">
+              Podsumowanie miesiąca {months[number - 1]}: {sumMonth(number)} zł
+            </div>
+          </caption>
+          <thead>
+            <tr>
+              <td>Lp.</td>
+              <td>Numer Faktury</td>
+              <td>Data Faktury</td>
+              <td>Wartość Faktury</td>
+              <td>Wartość narastająco</td>
+            </tr>
+          </thead>
+          {data
+            .filter(
+              (item) => new Date(item.data.date).getMonth() + 1 === number
+            )
+            .map((item, index) => (
+              <tbody key={index}>
+                <tr>
+                  <td>{index + 1}</td>
+                  <td>{item.data.number}</td>
+                  <td>{item.data.date}</td>
+                  <td className="records__amount">
+                    {getTotal(item.data.products)} zł
+                  </td>
+                  <td className="records__amount">{cumTotal[index]} zł</td>
+                </tr>
+              </tbody>
+            ))}
+          <tfoot>
+            <tr>
+              <td className="records__summary" colSpan={4}>
+                Podsumowanie:
+              </td>
+              <td className="records__summary">{sumMonth(number)} zł</td>
+            </tr>
+          </tfoot>
+        </table>
+      ) : (
+          <table id="records__tableSummary">
+            <caption>
+              <div className="records__total">
+                Podsumowanie roku: {totalYear()} zł
               </div>
-              <div>total Day: {getTotal(item.data.products)}</div>
-              <div>Narastająco: {cumTotal[index]} </div>
-            </li>
-          ))}
-      </ul>
+            </caption>
+            <thead>
+              <tr>
+                <td>Lp.</td>
+                <td>Miesiąc</td>
+                <td>Suma</td>
+              </tr>
+            </thead>
+            {months
+              .filter((_, index) => index !== summaryYearId)
+              .map((month, index) => (
+                <tbody key={index}>
+                  <tr>
+                    <td>{index + 1}</td>
+                    <td className="records__monthTd">{month}</td>
+                    <td className="records__amount">{totalMnoth[index]} zł</td>
+                  </tr>
+                </tbody>
+              ))}
+            <tfoot>
+              <tr>
+                <td className="records__summary" colSpan={2}>
+                  Razem:
+                </td>
+                <td className="records__summary">{totalYear()} zł</td>
+              </tr>
+            </tfoot>
+          </table>
+      )}
+      </div>
     </div>
   );
 }
