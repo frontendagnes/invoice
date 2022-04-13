00f734ecd20cd3aacc5ae61719cf311b0a6e3f9f
Agnieszka Kaminska <agnieszka.kaminska@ksiegarnia.edu.pl>
2022-04-13 12:24:54 +0200

    added footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Footer.css"
+function Footer() {
+  return (
+    <div className='footer'>
+        &copy; <a href="https://frontend-agnes.pl" title="frontend-agnes.pl" alt="frontend-agnes.pl">frontend-agnes</a>
+    </div>
+  )
+}
+
+export default Footer
