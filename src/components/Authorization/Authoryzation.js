1eddf1bf3610923e932173e82a2f057d5957af27
Agnieszka Kaminska <agnieszka.kaminska@ksiegarnia.edu.pl>
2022-04-21 12:15:51 +0200

    open graph image

--- src/components/Authorization/Authoryzation.js
+++ src/components/Authorization/Authoryzation.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./Authoryzation.css";
 import logo from "../../assets/pic/logo.webp";
 import { useStateValue } from "../../assets/utility/StateProvider";
