bff4314b5602cf1be46895b8743b7505d4fa9a9e
Agnieszka Kaminska <zabula81@o2.pl>
2024-05-02 12:08:21 +0200

    refactoring InfoYear component

--- src/components/InfoYear/index.jsx
+++ src/components/InfoYear/index.jsx
@@ -5,17 +5,17 @@ import { useStateValue } from "@/assets/utility/StateProvider";
 
 //components
 import Content from "./Content.jsx";
-import Approve from "./Approve.jsx";
 
 function InfoYear() {
   const [isYes, setIsYes] = useState(false);
   const [isInfo, setIsInfo] = useState(false);
   const [{ yearArray }, dispatch] = useStateValue();
+  const nextYear = new Date().getFullYear();
 
   useEffect(() => {
     if (
       yearArray.length > 0 &&
-      !yearArray.some((item) => item.data.year === 2024)
+      !yearArray.some((item) => item.data.year === nextYear)
     ) {
       setIsInfo(true);
       return;
@@ -23,15 +23,25 @@ function InfoYear() {
     setIsInfo(false);
   }, [yearArray]);
 
-  const addData = () => {
-    // updateNumber
+  const updateNumber = () => {};
+  const addData = () => {};
+
+  const addCurrentYear = () => {
+    addData();
+    updateNumber();
     console.log("Dodałam rok");
   };
+
   if (isInfo) {
     return (
       <div className="infoyear">
         {isYes ? (
-          <Approve handleClickNo={() => setIsYes(false)} addData={addData} />
+          <Content
+            text="Operacja spowoduje reset numeru faktury i dodanie nowego roku do bazy. Jesteś pewien że chcesz kontynuować"
+            handleClick={addCurrentYear}
+            handleClickNo={() => setIsYes(false)}
+            buttonYesTxt="Pewnie, dodaj rok"
+          />
         ) : (
           <Content
             text="Cześć, to ja Fakturka 2.0, wykryłam, że nie dodałeś/aś jeszcze aktualnego roku czy chcesz zrobić to teraz?"
