64e64a9752f8cbab235aa40ac2394f12abc9ce8d
Agnieszka Kaminska <zabula81@o2.pl>
2025-03-03 09:47:24 +0100

    changed creator url

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
-import "./Footer.css"
+import React from "react";
+import "./Footer.css";
 function Footer() {
   return (
-    <footer className='footer'>
-        &copy; <a href="https://frontend-agnes.pl" title="frontend-agnes.pl" alt="frontend-agnes.pl">frontend-agnes</a>
+    <footer className="footer">
+      &copy;{" "}
+      <a
+        href="https://frontend-agnes.web.app/"
+        title="frontend-agnes.web.app"
+        alt="frontend-agnes.web.app"
+      >
+        frontend-agnes
+      </a>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
